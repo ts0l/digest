3d9b5b65d68c4a31d47443500930bb56821fda24
Zach Burky <zach.burky@willowtreeapps.com>
2022-10-30 17:22:56 -0400

    Creates xcodeproj

---
+++ Digest/DigestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigestApp.swift
+//  Digest
+//
+//  Created by Zach Burky on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct DigestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
